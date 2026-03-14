a4a65f1562d926f141dbbcf107969e83e4d9e6e6
Just A. Guy <206441978+ufd-dev@users.noreply.github.com>
2026-03-14 16:40:41 -0700

    acceptable overlays; fully processed and sorted first 27

--- addoverlay.sh
+++ addoverlay.sh
@@ -1,22 +1,68 @@
 #!/bin/bash
 
-# Check if the first argument is empty
-if [ -z "$1" ]; then
-    echo "Error: No argument provided."
-    echo "Usage: $0 base_name_without_extension"
-    echo "Example: $0 gm_good-morning"
-    exit 1
-fi
+process_video() {
+    local input="$1"
+    local out_suffix="$2"
+    local extra_filters="$3"
+    shift 3 # we only want leftovers in $@ for use below
 
-SRC_ANIM=$1
-SRC_BASE_PATH="${SRC_ANIM%.*}"
-SRC_OVERLAY="$SRC_BASE_PATH.png"
-OUT="./out/${SRC_BASE_PATH##*/}.mp4"
+    local filename="${input%.*}"
+    filename=$(basename "$filename")
+    local outfile="out/${filename}${out_suffix}.mp4"
+    echo "Writing ${outfile}"
 
-ffmpeg \
-  -i $SRC_ANIM \
-  -i $SRC_OVERLAY \
-  -filter_complex "overlay=0:0:format=auto,format=yuv420p" \
-  -c:v libx264 -c:a aac -profile:v main -level 3.0 \
-  -movflags "faststart" \
-  $OUT
+    local inputs=(
+      -i "$input"
+      "$@"
+    )
+
+    local settings=(
+      -n                         # do not overwrite
+      -brand mp42                # avoids quicktime (early mp4) container
+      -c:v libx264               # recommended encoding
+      -crf 20                    # constant rate facotr; default 23; lower is higher quality
+      -an -sn -dn                # strip audio, subtitle, and data tracks
+      -profile:v main -level 3.0 # compatibility with more devices
+      -movflags "faststart"      # lets video play sooner on the web
+
+      # Force a keyframe every 2 seconds regardless of input FPS; better for quality & looping
+      -force_key_frames "expr:gte(t,n_forced*2)"
+
+      # format converts from paletted files like GIFs
+      # scale to set a max height of 720px
+      # extra_filters to receive watermark args from the caller
+      -filter_complex "[0:v]format=yuv420p,scale=-2:min(ih\,720)${extra_filters}"
+    )
+
+    ffmpeg "${inputs[@]}" "${settings[@]}" "$outfile"
+}
+
+# Format: "suffix:coordinates"
+# Left offset: 20 | Top offset: 20 | Bottom offset: 20 | Right offset: 10
+POSITIONS=(
+    "tl:20:20"
+    "tr:main_w-overlay_w-10:20"
+    "bl:20:main_h-overlay_h-20"
+    "br:main_w-overlay_w-10:main_h-overlay_h-20"
+)
+
+mkdir -p out
+
+shopt -s nocaseglob
+for file in src/*.{gif,mov,mp4,webp}; do
+    [ -e "$file" ] || continue
+
+    # reencoded with a max heigh of 720px
+    process_video "$file" "" ""
+
+    # create versions with the logo in each color and corner
+    for color in black white ; do
+      for pos in "${POSITIONS[@]}"; do
+          suffix="${pos%%:*}"
+          coords="${pos#*:}"
+          process_video "$file" "_${color}_${suffix}" ",overlay=${coords}" -i "overlay/logoTM-${color}.png"
+      done
+    done
+done
+shopt -u nocaseglob
+
